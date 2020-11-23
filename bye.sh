79d7a15e8bd49523258378ff4ad7526e8c6b51a8
Lii'zza Sulistio <liizzasulistio@gmail.com>
2020-11-23 22:11:16 +0700

    Create bye.sh

---
+++ bye.sh
@@ -0,0 +1,8 @@
+uml_mconsole SURABAYA halt
+uml_mconsole MALANG halt
+uml_mconsole TUBAN halt
+uml_mconsole MOJOKERTO halt
+uml_mconsole GRESIK halt
+uml_mconsole SIDOARJO halt
+uml_mconsole BANYUWANGI halt
+uml_mconsole MADIUN halt
